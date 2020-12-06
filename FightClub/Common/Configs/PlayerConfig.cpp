c858054b76465fa6e37a8d49bbb1c4fbeb8785d6
Эмиль Шархемуллин <emzilh@mail.ru>
2020-12-06 18:00:39 +0300

    Added creation of default player character if not found

--- FightClub/Common/Configs/PlayerConfig.cpp
+++ FightClub/Common/Configs/PlayerConfig.cpp
@@ -3,7 +3,14 @@
 Character* PlayerConfig::getCharacter()
 {
 	if (m_playerCharacter == nullptr)
+	{
 		m_playerCharacter = m_gameDataProcesser->load();
+
+		if (m_playerCharacter == nullptr)
+		{
+			m_playerCharacter = m_characterFactory->createDefaultPlayer();
+		}
+	}
 
 	return m_playerCharacter;
 }
--- FightClub/Common/Configs/PlayerConfig.h
+++ FightClub/Common/Configs/PlayerConfig.h
@@ -4,6 +4,7 @@
 #include <memory>
 
 #include "IPlayerConfig.h"
+#include "../../Characters/ICharacterFactory.h"
 #include "../../IO/Savers/GameDataProcesser.h"
 
 class PlayerConfig : public IPlayerConfig
@@ -12,10 +13,13 @@ private:
 	Character* m_playerCharacter;
 	
 	const std::shared_ptr<GameDataProcesser> m_gameDataProcesser;
+	const std::shared_ptr<ICharacterFactory> m_characterFactory;
 
 public:
-	PlayerConfig(const std::shared_ptr<GameDataProcesser>& gameDataProcesser) :
+	PlayerConfig(const std::shared_ptr<GameDataProcesser>& gameDataProcesser, 
+		const std::shared_ptr<ICharacterFactory>& characterFactory) :
 		m_gameDataProcesser{gameDataProcesser},
+		m_characterFactory{characterFactory},
 		m_playerCharacter{ nullptr }
 	{
 	}
